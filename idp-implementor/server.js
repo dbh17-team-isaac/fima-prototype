362df75d2f6715490655441100e2c0fd54d881dd
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2017-02-11 11:00:51 +0100

    idp-implementor: connect with blockchain node

--- idp-implementor/server.js
+++ idp-implementor/server.js
@@ -3,14 +3,24 @@
 const express = require('express');
 const Web3 = require('web3');
 
+// Configuration
+var config = {
+    listenPort: 8080,
+    parityUrl: "http://blockchain:8545"
+};
+
 // API
 const app = express();
 app.get('/test', function (req, res) {
-  res.send('Hello world\n');
+
+	var web3 = new Web3(new Web3.providers.HttpProvider(config.parityUrl));
+
+	res.send('version: ' + web3.version.node);
+
 });
 
-// Listen on 8080
-app.listen(8080);
+// Listen on specific port
+app.listen(config.listenPort);
 
 // Serve static files from public/
 app.use(express.static('public'));
