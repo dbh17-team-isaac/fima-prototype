4b0deefa44241cf01d5d46f13356fbd25caed0ce
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2017-02-11 18:33:08 +0100

    pdm-ui: add stubs for requests

--- pdm-ui/lib/api-stubs.js
+++ pdm-ui/lib/api-stubs.js
@@ -7,7 +7,10 @@ module.exports = {
     },
 
     createRequest: function(askIdentityId, targetIdentityId, requestFields) {
-        return {};
+        return {
+            result: "success",
+            requestId: 12345
+        };
     },
 
     getAttributesForIdentity: function(identityId) {
@@ -30,6 +33,14 @@ module.exports = {
                     },
                     value: "1987-06-28"
                 },
+                {
+                    field: {
+                        UUID: "4182654f-72f3-4f88-87da-f569e3fff13a",
+                        type: "string",
+                        caption: "Birth place"
+                    },
+                    value: "Utrecht"
+                },
             ]
         };
     },
@@ -39,6 +50,26 @@ module.exports = {
     },
 
     getRequestsForIdentity: function(identityId) {
-        return {};
+        return {
+            requests: [
+                {
+                    requestId: 12345,
+                    askIdentity: {
+                        identityId: 999,
+                        description: "Jan Jansen"
+                    },
+                    requestFields: [
+                        {
+                            field: {
+                                UUID: "7ff209a8-aa7e-45b0-9d0a-e5259ca73a4f",
+                                type: "string",
+                                caption: "Birth date"
+                            },
+                            required: true
+                        }
+                    ]
+                }
+            ]
+        };
     }
 };
