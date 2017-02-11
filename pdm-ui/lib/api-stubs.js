059706c0792de9a2ec381f4411f6a299dbf68dde
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2017-02-11 18:17:30 +0100

    pdm-ui: add getAttributesForIdentity stub

--- pdm-ui/lib/api-stubs.js
+++ pdm-ui/lib/api-stubs.js
@@ -11,7 +11,27 @@ module.exports = {
     },
 
     getAttributesForIdentity: function(identityId) {
-        return {};
+        return {
+            identityId: identityId,
+            attributes: [
+                {
+                    field: {
+                        UUID: "16a8053c-329e-41cb-ab9d-87c6f4ee2d6a",
+                        type: "string",
+                        caption: "Full name"
+                    },
+                    value: "Jan Jansen"
+                },
+                {
+                    field: {
+                        UUID: "7ff209a8-aa7e-45b0-9d0a-e5259ca73a4f",
+                        type: "string",
+                        caption: "Birth date"
+                    },
+                    value: "1987-06-28"
+                },
+            ]
+        };
     },
 
     getAuthorizationsForIdentity: function(identityId) {
