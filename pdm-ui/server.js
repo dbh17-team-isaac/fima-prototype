953e96898d240e8a119349f2fa78ec79f677d01a
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2017-02-11 17:24:55 +0100

    pdm-ui: add API endpoints for prototype

--- pdm-ui/server.js
+++ pdm-ui/server.js
@@ -11,12 +11,39 @@ var config = {
 
 // API
 const app = express();
-app.get('/test', function (req, res) {
 
-	var web3 = new Web3(new Web3.providers.HttpProvider(config.parityUrl));
+// API: retrieve identity attributes
+app.get('/api/v1/identity/:identityId/attributes', function(req, res) {
+    var identityId = req.params.identityId;
+    res.send('TODO');
+});
 
-	res.send('version: ' + web3.version.node);
+// API: retrieve identity authorizations
+app.get('/api/v1/identity/:identityId/authorization', function(req, res) {
+    var identityId = req.params.identityId;
+    res.send('TODO'); 
+});
 
+// API: retrieve identity requests
+app.get('/api/v1/identity/:identityId/request', function(req, res) {
+    var identityId = req.params.identityId;
+    res.send('TODO'); 
+});
+
+// API: confirm request
+app.post('/api/v1/identity/:identityId/request/:requestId', function(req, res) {
+    var identityId = req.params.identityId;
+    var requestId = req.params.requestId;
+    res.send('TODO');
+});
+
+// API: request authorization
+app.post('/api/v1/request', function(req, res) {
+    // askIdentityId
+    // targetIdentityId
+    // requestFields
+    console.log(req.body);
+    res.send('TODO');
 });
 
 // Listen on specific port
@@ -24,4 +51,3 @@ app.listen(config.listenPort);
 
 // Serve static files from public/
 app.use(express.static('public'));
-
