5b2277892d80394e77ff9e407cab3b803201d69b
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2017-02-11 10:26:21 +0100

    idp-implementor: serve static files

--- idp-implementor/server.js
+++ idp-implementor/server.js
@@ -3,11 +3,15 @@
 const express = require('express');
 const Web3 = require('web3');
 
-// App
+// API
 const app = express();
-app.get('/', function (req, res) {
+app.get('/test', function (req, res) {
   res.send('Hello world\n');
 });
 
+// Listen on 8080
 app.listen(8080);
+
+// Serve static files from public/
+app.use(express.static('public'));
 
