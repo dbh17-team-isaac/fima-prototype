f65d65f811faa90e46ba1dbbeedaa1cdb88d6187
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2017-02-11 21:40:55 +0100

    pdm-ui: implement PDM state stubs

--- pdm-ui/_dev/js/angular.pdm.app.js
+++ pdm-ui/_dev/js/angular.pdm.app.js
@@ -1,8 +1,23 @@
 var app = angular.module('pdm', []);
 
-var identityId = "12345"
+// Initialize PDM state from sessionStorage
+if (!sessionStorage.getItem('PDM-IdentityId')) {
+    var identityId = prompt('Identity ID:', '1');
+    var targetIdentityId = prompt('Target Identity ID:', '2');
+
+    sessionStorage.setItem('PDM-IdentityId', identityId);
+    sessionStorage.setItem('PDM-TargetIdentityId', targetIdentityId);
+}
 
 // Global functionality
+function getIdentityId() {
+    return sessionStorage.getItem('PDM-IdentityId');
+}
+
+function getTargetIdentityId() {
+    return sessionStorage.getItem('PDM-TargetIdentityId');
+}
+
 function resetStubs() {
     var $http = angular.injector(["ng"]).get("$http");
     $http.post('/api/v1/stubs/reset').then(function() {
--- pdm-ui/_dev/js/angular.pdm.personal-data.js
+++ pdm-ui/_dev/js/angular.pdm.personal-data.js
@@ -2,18 +2,18 @@ angular.module('pdm').controller('pdmPersonalDataCtrl', ['$scope', '$http', func
 
     $scope.editMode = false;
 
-    $http.get('/api/v1/identity/' + identityId + '/attributes').then(function(response) {
+    $http.get('/api/v1/identity/' + getIdentityId() + '/attributes').then(function(response) {
 		$scope.attributes = response.data.attributes;
 	});
 
     $scope.toggleEditMode = function() {
         $scope.editMode = !$scope.editMode;
-    }
+    };
 
     $scope.savePersonalDetails = function() {
         // TODO save
         $scope.editMode = false;
-    }
+    };
 
     // Global functionality
     $scope.resetStubs = resetStubs;
