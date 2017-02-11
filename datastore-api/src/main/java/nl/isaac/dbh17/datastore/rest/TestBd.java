feef4e7e62e9ec796ee741ee67a8a79716046412
Jorith vd Heuvel <jorith@gmail.com>
2017-02-11 09:49:43 +0100

    Add Hello world to datastore-api

---
+++ datastore-api/src/main/java/nl/isaac/dbh17/datastore/rest/TestBd.java
@@ -0,0 +1,16 @@
+package nl.isaac.dbh17.datastore.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("test")
+@Produces(MediaType.APPLICATION_JSON)
+public class TestBd {
+
+	@GET @Path("/")
+	public String test() {
+		return "Hello world!";
+	}
+}
