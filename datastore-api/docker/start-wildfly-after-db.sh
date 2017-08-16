ac382fad86f8468d98a3ad3d563ef23c2b4ab3a9
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2017-08-16 21:18:36 +0200

    Prevent connection refused errors from netcat

--- datastore-api/docker/start-wildfly-after-db.sh
+++ datastore-api/docker/start-wildfly-after-db.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "Waiting for the database to come up..."
-until nc -w 2 db 3306; do sleep 1; done
+until nc -w 2 db 3306 2> /dev/null; do sleep 1; done
 
 /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0
+
